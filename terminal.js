687f94f6cd0ebc55d32aaf7ce2e9b220773013a3
ArthurDetko <detkoarthur@gmail.com>
2021-09-19 18:03:10 +0200

    Add function "showname"

---
+++ terminal.js
@@ -0,0 +1,8 @@
+function showname (name){
+    if (name == "Arthur"){
+        console.log("привет "+ name);
+        } else {
+            console.log("нет такого имени");
+    } 
+    }
+showname("Arthur");
